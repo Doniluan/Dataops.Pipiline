cee5d546f8aabed5d18eae79f09c49273259d10c
Luan <luan_zeti@hotmail.com>
2024-08-15 00:58:35 +0100

    Adicionando código para teste local e reordenando pastas

---
+++ src/hospital/ingestion_cardiovascular.py
@@ -0,0 +1,35 @@
+import logging
+from pyspark.sql import SparkSession
+from delta import configure_spark_with_delta_pip
+
+def setup_session():
+    builder = SparkSession.builder.appName("Ingestão Cardio") \
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+        .config("spark.hadoop.io.nativeio.enabled", "false") \
+        .config("spark.hadoop.fs.native.enabled", "false") \
+        .config("spark.jars.packages", "io.delta:delta-core_2.12:2.4.0")
+
+    return configure_spark_with_delta_pip(builder).getOrCreate()
+
+def read_csv(spark, path="C:\\Projeto_DataOps\\Dataops.Pipiline\\data_sources\\cardiovascular.csv"):
+    logging.info("Realizando leitura do arquivo")
+    return spark.read.format("csv").option("header", "true").load(path)
+
+def rename_columns(df):
+    logging.info("Renomeando colunas")
+    return df.withColumnRenamed("height_(cm)", "height_cm").withColumnRenamed("Weight_(kg)", "Weight_kg")
+
+def save_delta(df):
+    logging.info("Armazenando dados")
+    return df.write.format("delta").mode("overwrite").option("mergeSchema", "true").partitionBy("General_Health").save("C:\\Projeto_DataOps\\Dataops.Pipiline\\storage\\hospital\\rw\\cardiovascular\\")
+
+def main():
+    spark = setup_session()
+    df = read_csv(spark)
+    df = rename_columns(df)
+    save_delta(df)
+    spark.stop()
+
+if __name__ == "__main__":
+    main()
