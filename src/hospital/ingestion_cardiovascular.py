84d24326f183abc1dcdfdc407272e40e77897bea
Luan <luan_zeti@hotmail.com>
2024-08-15 19:39:22 +0100

    AAAAAAA

--- src/hospital/ingestion_cardiovascular.py
+++ src/hospital/ingestion_cardiovascular.py
@@ -1,6 +1,8 @@
 import logging
 from pyspark.sql import SparkSession
 from delta import configure_spark_with_delta_pip
+from pathlib import Path
+
 
 def setup_session():
     builder = SparkSession.builder.appName("Ingestão Cardio") \
@@ -12,24 +14,41 @@ def setup_session():
 
     return configure_spark_with_delta_pip(builder).getOrCreate()
 
-def read_csv(spark, path="C:\\Projeto_DataOps\\Dataops.Pipiline\\data_sources\\cardiovascular.csv"):
-    logging.info("Realizando leitura do arquivo")
-    return spark.read.format("csv").option("header", "true").load(path)
+
+def read_csv(spark, path=None):
+    if path is None:
+        path = Path("data_sources") / "cardiovascular.csv"
+    else:
+        path = Path(path)
+
+    logging.info("Realizando leitura do arquivo no caminho: %s", path)
+
+    if not path.exists():
+        raise FileNotFoundError(f"O arquivo não foi encontrado no caminho especificado: {path}")
+
+    df = spark.read.format("csv").option("header", "true").load(str(path))
+    df.show(truncate=False)  # Imprime o DataFrame na tela
+    return df
+
 
 def rename_columns(df):
     logging.info("Renomeando colunas")
     return df.withColumnRenamed("height_(cm)", "height_cm").withColumnRenamed("Weight_(kg)", "Weight_kg")
 
-def save_delta(df):
+
+def save_delta(df, output_path=Path("storage/hospital/rw/cardiovascular/")):
     logging.info("Armazenando dados")
-    return df.write.format("delta").mode("overwrite").option("mergeSchema", "true").partitionBy("General_Health").save("C:\\Projeto_DataOps\\Dataops.Pipiline\\storage\\hospital\\rw\\cardiovascular\\")
+    return df.write.format("delta").mode("overwrite").option("mergeSchema", "true").partitionBy("General_Health").save(str(output_path))
+
 
 def main():
     spark = setup_session()
-    df = read_csv(spark)
+    absolute_path = "C:/Projeto_DataOps/Dataops.Pipiline/data_sources/cardiovascular.csv"
+    df = read_csv(spark, absolute_path)
     df = rename_columns(df)
     save_delta(df)
     spark.stop()
 
+
 if __name__ == "__main__":
-    main()
+    main()  # dessa vez vai
