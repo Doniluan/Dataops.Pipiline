f614188f22d83dfb286d51acf7581cceebcb12c0
Luan <luan_zeti@hotmail.com>
2024-08-15 03:30:22 +0100

    Arrumando o notebook e o ingestion

--- src/hospital/ingestion_cardiovascular.py
+++ src/hospital/ingestion_cardiovascular.py
@@ -2,6 +2,7 @@ import logging
 from pyspark.sql import SparkSession
 from delta import configure_spark_with_delta_pip
 
+
 def setup_session():
     builder = SparkSession.builder.appName("Ingestão Cardio") \
         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
@@ -12,17 +13,21 @@ def setup_session():
 
     return configure_spark_with_delta_pip(builder).getOrCreate()
 
+
 def read_csv(spark, path="C:\\Projeto_DataOps\\Dataops.Pipiline\\data_sources\\cardiovascular.csv"):
     logging.info("Realizando leitura do arquivo")
     return spark.read.format("csv").option("header", "true").load(path)
+
 
 def rename_columns(df):
     logging.info("Renomeando colunas")
     return df.withColumnRenamed("height_(cm)", "height_cm").withColumnRenamed("Weight_(kg)", "Weight_kg")
 
+
 def save_delta(df):
     logging.info("Armazenando dados")
     return df.write.format("delta").mode("overwrite").option("mergeSchema", "true").partitionBy("General_Health").save("C:\\Projeto_DataOps\\Dataops.Pipiline\\storage\\hospital\\rw\\cardiovascular\\")
+
 
 def main():
     spark = setup_session()
@@ -31,5 +36,7 @@ def main():
     save_delta(df)
     spark.stop()
 
+
 if __name__ == "__main__":
     main()
+    
