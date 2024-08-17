1533a094938a64d1eb32827f562015af0e68309e
Luan <luan_zeti@hotmail.com>
2024-08-17 04:19:14 +0100

    UHHUUU

--- src/notebook/ingestion_cardiovascular.py
+++ src/notebook/ingestion_cardiovascular.py
@@ -1,9 +1,6 @@
 # Databricks notebook source.
-
 # MAGIC %md
-
 # MAGIC #Ingestão de Dados ELT
-
 # MAGIC Conjunto de dados de previsão de risco de doenças cardiovasculares
 
 # COMMAND ------------
@@ -29,7 +26,6 @@ display(dbutils.fs.ls("/tmp/"))
 # COMMAND -------------
 
 #MAGIC %md
-
 # MAGIC Extraindo dados/Realizando a leitura
 
 # COMMAND ------------
@@ -49,7 +45,6 @@ df.select("General_Health").distinct().display()
 df.printSchema()
 
 # MAGIC %md
-
 # MAGIC # Rename Cols
 
 # COMMAND ------------
@@ -57,7 +52,6 @@ df.printSchema()
 df = df.withColumnRenamed("Height_(cm)", "Height_cm").withColumnRenamed("Weight_(kg)", "Weight_kg")
 
 # MAGIC %md
-
 # MAGIC Realizando o armazenamento de dados
 
 # COMMAND ------------
@@ -65,17 +59,14 @@ df = df.withColumnRenamed("Height_(cm)", "Height_cm").withColumnRenamed("Weight_
 df.write.format("delta").mode("overwrite").option("mergeSchema", True).partitionBy("General_Health").save("/hospital/rw/suus/cardiovascular/")
 
 # MAGIC %md
-
 # MAGIC Criando database e tabela pelo delta location
 
 # COMMAND ------------
 
 # MAGIC %sql
-
 # MAGIC CREATE DATABASE IF NOT EXISTS db_hospital
 
 # COMMAND ------------
 
 # MAGIC %sql
-
 # MAGIC CREATE TABLE IF NOT EXISTS db_hospital.cardiovascular_diseasess LOCATION "gs://databricks-46083817904810/46083817904810/hospital/rw/suus/cardiovascular"
